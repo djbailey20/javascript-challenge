948bdff636eb481c759fd6777de640562d141896
Deshaune Bailey <deshaune.bailey@gmail.com>
2020-03-21 10:02:02 -0400

    added part 2

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,49 +1,70 @@
 // from data.js
 var tableData = data;
-
+var filterInfo = {
+  datetime: "",
+  city: "",
+  state: "",
+  country: "",
+  shape: ""
+};
 // YOUR CODE HERE!
 
-const loadTable = e => {
-  table = d3.select("#table-area").html("");
-  table = d3
-    .select("#table-area")
+const loadTable = tableInfo => {
+  d3.select("#table-area").html("");
+  d3.select("#table-area")
     .append("table")
-    .append("tbody");
-  d3.select("table").attr("class", "table table-striped");
+    .attr("class", "table table-striped");
+
   th = ["Date", "City", "State", "Country", "Shape", "Duration", "Comments"];
   head = d3
     .select("table")
     .append("thead")
     .append("tr");
   th.forEach(title => {
-    head.append("th").text(title);
+    head
+      .append("th")
+      .text(title)
+      .attr("class", "table-head");
   });
-  e.map(ufo => {
+  table = d3.select("table").append("tbody");
+  tableInfo.map(ufo => {
     tr = table.append("tr");
-    Object.entries(ufo).forEach(([key, value]) => {
+    Object.values(ufo).forEach(value => {
       tr.append("td").text(value);
     });
   });
 };
-// d3.select("#datetime").on("keyup", () => {
-//   console.log(d3.event.target.value);
-//   console.log(d3.select("#datetime").attr("value"));
+
+d3.selectAll(".form-control").on("keyup", () => {
+  filterInfo[d3.event.target.id] = d3.event.target.value;
+  // console.log(filterInfo);
+});
+
+// d3.select("#filter-btn").on("click", () => {
+//   date = d3.select("#datetime").node().value;
+//   if (date == "") {
+//     loadTable(tableData);
+//   } else {
+//     filterData = data.filter(e => e.datetime == date);
+//     loadTable(filterData);
+//     // console.log(date);
+//   }
+//   //   handleClick();
 // });
 
-// const handleClick = () => {
-//   d3.selectAll("input").each(console.log(this));
-// };
+function fInfo(info) {
+  return (
+    (filterInfo["datetime"] == info.datetime || filterInfo["datetime"] == "") &&
+    (filterInfo["city"] == info.city || filterInfo["city"] == "") &&
+    (filterInfo["state"] == info.state || filterInfo["state"] == "") &&
+    (filterInfo["country"] == info.country || filterInfo["country"] == "") &&
+    (filterInfo["shape"] == info.shape || filterInfo["shape"] == "")
+  );
+}
 
 d3.select("#filter-btn").on("click", () => {
-  date = d3.select("#datetime").node().value;
-  if (date == "") {
-    loadTable(tableData);
-  } else {
-    filterData = data.filter(e => e.datetime == date);
-    loadTable(filterData);
-    // console.log(date);
-  }
-  //   handleClick();
+  filterData = data.filter(fInfo);
+  loadTable(filterData);
 });
 
 loadTable(tableData);
